592906a3b7029c95b686eb9ae2b1e0d3f79b25e1
Ivan Yakovlev <ivanyakovlev.jd@gmail.com>
2017-04-20 16:44:30 +0700

    1310 complite

--- 2.JavaCore/src/com/javarush/task/task13/task1309/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1309/Solution.java
@@ -7,4 +7,10 @@ package com.javarush.task.task13.task1309;
 public class Solution {
     public static void main(String[] args) throws Exception {
     }
+    interface Movable{
+        public Double speed();
+    }
+    interface Flyable extends Movable{
+        public  Double speed(Flyable a);
+    }
 }
