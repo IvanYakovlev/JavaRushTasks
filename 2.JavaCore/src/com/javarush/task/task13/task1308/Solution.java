9d6f37b321a00e606f203e282f02d6e7704bde00
Ivan Yakovlev <ivanyakovlev.jd@gmail.com>
2017-04-20 15:44:19 +0700

    1308 complete

--- 2.JavaCore/src/com/javarush/task/task13/task1308/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1308/Solution.java
@@ -6,5 +6,13 @@ package com.javarush.task.task13.task1308;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+    }
+    interface Person{
+          public boolean isAlive();
+
+    }
+    interface Presentable extends Person{
+
     }
 }
