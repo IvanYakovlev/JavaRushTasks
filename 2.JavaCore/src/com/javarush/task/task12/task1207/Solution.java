c88cbad767a16ec5b0fa6a93e3e2f57f9811418f
Ivan Yakovlev <ivanyakovlev.jd@gmail.com>
2017-04-20 14:13:41 +0700

    merge

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a=4;
+        Integer b=5;
+        print(a);
+        print(b);
+    }
+    public static void print(int a){}
+    public static void print(Integer b){}
+    //Напишите тут ваши методы
+}
