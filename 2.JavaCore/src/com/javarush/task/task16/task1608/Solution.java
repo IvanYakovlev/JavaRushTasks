15c99b186539992ec8ddc0d22ea375deca333e1a
ivanyakovlev <ivanyakovlev.jd@gmail.com>
2017-05-11 17:33:05 +0700

    ff

--- 2.JavaCore/src/com/javarush/task/task16/task1608/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1608/Solution.java
@@ -10,7 +10,9 @@ public class Solution {
 
     public static void main(String[] args) throws InterruptedException {
         Politician ivanov = new Politician("Иванов");
+        ivanov.join();
         Politician petrov = new Politician("Петров");
+
         Politician sidorov = new Politician("Сидоров");
 
         while (ivanov.getCountSpeeches() + petrov.getCountSpeeches() + sidorov.getCountSpeeches() < totalCountSpeeches) {
