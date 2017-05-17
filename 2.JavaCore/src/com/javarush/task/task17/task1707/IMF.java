142a2d3bb391338aa7a7c8df8bdc0a873bbf504c
ivanyakovlev <ivanyakovlev.jd@gmail.com>
2017-05-17 17:52:53 +0700

    1707

--- 2.JavaCore/src/com/javarush/task/task17/task1707/IMF.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1707/IMF.java
@@ -6,6 +6,9 @@ public class IMF {
     private static IMF imf;
 
     public static IMF getFund() {
+        synchronized (IMF.class){
+          if (imf==null) imf=new IMF(); else return imf;
+        }
         //add your code here - добавь код тут
         return imf;
     }
