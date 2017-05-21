9bde26f8a243aa8a026053dd597e79d0a458bbbb
IvanYakovlev <IvanYakovlev.jd@gmail.com>
2017-05-21 23:53:33 +0700

    fiss

--- 2.JavaCore/src/com/javarush/task/task17/task1721/CorruptedDataException.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1721/CorruptedDataException.java
@@ -3,4 +3,5 @@ package com.javarush.task.task17.task1721;
 import java.io.IOException;
 
 public class CorruptedDataException extends IOException {
+
 }
--- 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task17.task1721;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,10 +14,23 @@ public class Solution {
     public static List<String> allLines = new ArrayList<String>();
     public static List<String> forRemoveLines = new ArrayList<String>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+       try {
+           BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+           allLines.add(reader.readLine());
+           forRemoveLines.add(reader.readLine());
+           reader.close();
+           joinData();
+       } catch (CorruptedDataException e) {}
     }
 
-    public void joinData () throws CorruptedDataException {
+    public static void joinData() throws IOException {
+         if (allLines.contains(forRemoveLines)){
+            allLines.remove(forRemoveLines);
 
+        } else {
+            allLines.remove(allLines);
+            new CorruptedDataException();
+        }
     }
 }
