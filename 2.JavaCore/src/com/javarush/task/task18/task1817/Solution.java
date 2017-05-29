5a5079dcf7b8df3ca89c7ff6ecd420f5295ff8e7
ivanyakovlev <ivanyakovlev.jd@gmail.com>
2017-05-29 17:57:53 +0700

    fix

--- 2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
@@ -4,8 +4,32 @@ package com.javarush.task.task18.task1817;
 Пробелы
 */
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
+        FileReader f = new FileReader("C:/1.txt");
+
+        int count = 0,bs = 0;
+        while (f.ready()) {
+            char s = (char) f.read();
+            count++;
+            if (s ==' ')
+                bs++;
+        }
+        double a1=(double) count;
+        double a2=(double) bs;
+        f.close();
+        double b = a2/(a1-a2)*100;
+
+        System.out.printf("%.2f", b);
+
 
     }
+
 }
+
