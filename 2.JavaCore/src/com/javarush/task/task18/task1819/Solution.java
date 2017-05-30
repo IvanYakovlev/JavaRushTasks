4be7318a93fd725c6443ae3b94a11c55fb2c67be
ivanyakovlev <ivanyakovlev.jd@gmail.com>
2017-05-30 17:11:18 +0700

    1820

--- 2.JavaCore/src/com/javarush/task/task18/task1819/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1819/Solution.java
@@ -4,8 +4,36 @@ package com.javarush.task.task18.task1819;
 Объединение файлов
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.ArrayList;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file1name = reader.readLine();
+        String file2name = reader.readLine();
+        reader.close();
+        FileInputStream file1r = new FileInputStream(file1name);
+        FileInputStream file2 = new FileInputStream(file2name);
+        ArrayList<Integer> array = new ArrayList<>();
+        reader.close();
+        while (file2.available()>0){
+            int a = file2.read();
+            array.add(a);
+        }
+
+        while (file1r.available()>0){
+            int a = file1r.read();
+            array.add(a);
+        }
+
+        file1r.close();
+        FileOutputStream file1w = new FileOutputStream(file1name);
+        for (int i=0;i<array.size();i++){
+            file1w.write(array.get(i));
+        }
+
+        file1w.close();
+        file2.close();
     }
 }
--- 2.JavaCore/src/com/javarush/task/task18/task1820/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1820/Solution.java
@@ -4,8 +4,23 @@ package com.javarush.task.task18.task1820;
 Округление чисел
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream file1 = new FileInputStream(reader.readLine());
+        FileOutputStream file2 = new FileOutputStream(reader.readLine());
+        StringBuffer sb = new StringBuffer();
+        while (file1.available()>0){
+            char b = (char) file1.read();
+            sb.append(b);
+            if (b==' '){
+                System.out.println(sb);
+                }
+
+
+
+        }
     }
 }
