3c69315db4aa9a72cfa8ded394683eb5cd69d304
ivanyakovlev <ivanyakovlev.jd@gmail.com>
2017-06-02 17:31:11 +0700

    fix

--- 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
@@ -4,7 +4,30 @@ package com.javarush.task.task18.task1822;
 Поиск данных внутри файла
 */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileReader file  = new FileReader(reader.readLine());
+        Map<Integer, Product> map = new HashMap<>();
+
+
+    }
+}
+class Product {
+    String name;
+    Double price;
+    int quantity;
+
+    public Product(String name, Double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
     }
 }
