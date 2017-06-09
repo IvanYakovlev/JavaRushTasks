44ac6ed2684c278f60d3f844b7adbcdf342b004c
ivanyakovlev <ivanyakovlev.jd@gmail.com>
2017-06-09 17:21:40 +0700

    1825

--- 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.task.task18.task1823;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -15,29 +12,48 @@ import java.util.TreeMap;
 public class Solution {
     public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String a;
+        while (!(a=reader.readLine()).equals("exit")) {
+            ReadThread thread = new ReadThread(a);
+            thread.run();
+        }
 
     }
 
     public static class ReadThread extends Thread {
         FileInputStream fileInputStream;
+        public String fileName;
         public ReadThread(String fileName) throws FileNotFoundException {
-          fileInputStream  = new FileInputStream(fileName); //implement constructor body
+          fileInputStream  = new FileInputStream(fileName);
+          this.fileName=fileName;//implement constructor body
         }
 
         @Override
         public void run() {
 
-            Map<Character, Integer> map=new TreeMap<>();
+            Map<Integer, Integer> map=new TreeMap<>();
             try {
                 while (fileInputStream.available()>0){
-                    char read = (char) fileInputStream.read();
+                    int read = (int) fileInputStream.read();
                     if (map.containsKey(read)){
                         map.put(read, map.get(read)+1);}else {map.put(read, 1);}
-                    for (Map.Entry<Character,Integer> entry: map.entrySet())
-                        System.out.println(entry.getKey()+" "+entry.getValue());
-                    fileInputStream.close();
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            int max=0;
+            int maxbyte=0;
+            for (Map.Entry<Integer,Integer> entry: map.entrySet()){
+                if (entry.getValue()>max){
+                max=entry.getValue();
+                maxbyte=entry.getKey();
+                }
+            }
+            resultMap.put(fileName,maxbyte);
+            try {
+                fileInputStream.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
@@ -4,7 +4,22 @@ package com.javarush.task.task18.task1824;
 Файлы и исключения
 */
 
+import java.io.*;
+import java.lang.reflect.Field;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String a=null;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true){
+            try {
+                a = reader.readLine();
+                FileReader reader1 = new FileReader(a);
+            } catch (FileNotFoundException e) {
+                reader.close();
+               break;
+            }
+        }
+        System.out.print(a);
     }
 }
