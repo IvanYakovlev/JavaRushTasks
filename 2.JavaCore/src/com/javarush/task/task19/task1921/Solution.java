fda49b4baa24be887f40f29210786b5b0e057bee
ivanyakovlev <ivanyakovlev.jd@gmail.com>
2017-07-04 17:40:12 +0700

    1923

--- 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
@@ -6,8 +6,11 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 /* 
@@ -17,23 +20,27 @@ import java.util.List;
 public class Solution {
     public static final List<Person> PEOPLE = new ArrayList<Person>();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         BufferedReader reader = new BufferedReader(new FileReader("C:/1.txt"));
         while (reader.ready()){
             int year;
             int month;
             int day;
-            String name = null;
+            String name = "";
             String[] a = reader.readLine().split(" ");
-            year= Integer.parseInt(a[a.length]);
-            month= Integer.parseInt(a[a.length-1]);
-            day= Integer.parseInt(a[a.length-2]);
-            for (int i=0;i<a.length-2;i++){
-                name=a[i]+" ";
+            year= Integer.parseInt(a[a.length-1]);
+            month= Integer.parseInt(a[a.length-2]);
+            day= Integer.parseInt(a[a.length-3]);
+            Date date1 = new GregorianCalendar(year, month-1, day).getTime();
+            for (int i=0;i<a.length-3;i++){
+                if (i==a.length-4){name=name+a[i];}else {name=name+a[i]+" ";}
             }
-            PEOPLE.add(new Person(name, ));
-            }
-            System.out.println();
+            PEOPLE.add(new Person(name, date1));
+
         }
+        reader.close();
+        for (Person p :PEOPLE)
+            System.out.println(p.getName()+" "+p.getBirthday());
     }
+}
 
--- 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -1,5 +1,9 @@
 package com.javarush.task.task19.task1922;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +20,23 @@ public class Solution {
         words.add("В");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new FileReader(reader1.readLine()));
+        reader1.close();
+        while (reader.ready()){
+            String read=reader.readLine();
+            String[] a =read.split("[\\p{P}\\s\\t\\n\\r]");
+            int count=0;
+           for (String line : a) {
+               for (String word : words) {
+                   if (line.trim().equals(word)) ;
+                   count+=1;
+               }
 
+           }
+            if (count==2) System.out.println(read);
+        }
+        reader.close();
     }
 }
