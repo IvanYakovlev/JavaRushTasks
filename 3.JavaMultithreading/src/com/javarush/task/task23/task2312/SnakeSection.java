47943e5df19fa60616bf44a898bbb28b28b02204
ivanyakovlev <ivanyakovlev.jd@gmail.com>
2017-08-09 17:34:01 +0700

    Arkanoid start

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -4,25 +4,6 @@ public class SnakeSection {
     private int x;
     private int y;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        SnakeSection that = (SnakeSection) o;
-
-        if (x != that.x) return false;
-        return y == that.y;
-
-    }
-
-    @Override
-    public int hashCode() {
-
-        int result = x;
-        result = 31 * result + y;
-        return result;
-    }
 
     public SnakeSection(int x, int y) {
         this.x = x;
@@ -37,6 +18,23 @@ public class SnakeSection {
         return y;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        SnakeSection that = (SnakeSection) o;
 
+        if (x != that.x) return false;
+        if (y != that.y) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -0,0 +1,67 @@
+package com.javarush.task.task24.task2413;
+
+import java.util.List;
+
+public class Arkanoid {
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
+    }
+
+    public Ball getBall() {
+
+        return ball;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    private int width, height;
+    private Ball ball;
+    private Stand stand;
+    private List<Brick> bricks;
+
+    public Arkanoid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public  void run(){}
+
+    public void move(){}
+
+    static Arkanoid game;
+
+    public static void main(String[] args) {
+
+    }
+}
