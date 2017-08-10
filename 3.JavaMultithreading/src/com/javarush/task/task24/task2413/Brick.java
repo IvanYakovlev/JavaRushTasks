7236e240a84909d7fd7dc25be19eff265f615a71
ivanyakovlev <ivanyakovlev.jd@gmail.com>
2017-08-10 17:53:18 +0700

    asd

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -1,4 +1,22 @@
 package com.javarush.task.task24.task2413;
 
-public class Brick {
+public class Brick extends BaseObject {
+    public Brick(double x, double y, double radius) {
+        super(x, y, radius);
+    }
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public boolean isIntersec(BaseObject o) {
+        return false;
+    }
 }
