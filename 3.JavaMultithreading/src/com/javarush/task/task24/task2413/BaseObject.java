abf87f5276d78183643d04cc5e09feb5587ac26d
ivanyakovlev <ivanyakovlev.jd@gmail.com>
2017-08-11 16:58:24 +0700

    arkan done

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
@@ -1,44 +1,69 @@
 package com.javarush.task.task24.task2413;
 
+/**
+ * Базовый класс для всех объектов игры.
+ */
 public abstract class BaseObject {
-    private double x,y,radius;
+    //координаты
+    protected double x;
+    protected double y;
+    //радиус объекта
+    protected double radius;
 
-    public void setX(double x) {
+    protected BaseObject(double x, double y, double radius) {
         this.x = x;
-    }
-
-    public void setY(double y) {
         this.y = y;
-    }
-
-    public void setRadius(double radius) {
         this.radius = radius;
     }
 
     public double getX() {
-
         return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
     }
 
     public double getY() {
         return y;
     }
 
+    public void setY(double y) {
+        this.y = y;
+    }
+
     public double getRadius() {
         return radius;
     }
 
-    public BaseObject(double x, double y, double radius) {
-        this.x = x;
-        this.y = y;
+    public void setRadius(double radius) {
         this.radius = radius;
     }
 
-    public abstract void move();
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    abstract void draw(Canvas canvas);
 
-    public abstract void draw(Canvas canvas);
+    /**
+     * Двигаем себя на один ход.
+     */
+    abstract void move();
 
-    public boolean isIntersec(BaseObject o){
+    /**
+     * Проверяем - не выходит ли (x,y) за границы.
+     */
+    void checkBorders(double minx, double maxx, double miny, double maxy) {
+        if (x < minx) x = minx;
+        if (x > maxx) x = maxx;
+        if (y < miny) y = miny;
+        if (y > maxy) y = maxy;
+    }
+
+    /**
+     * Проверяем - пересекаются ли переданный(o) и наш(this) объекты.
+     */
+    boolean isIntersec(BaseObject o) {
         double dx = x - o.x;
         double dy = y - o.y;
         double destination = Math.sqrt(dx * dx + dy * dy);
